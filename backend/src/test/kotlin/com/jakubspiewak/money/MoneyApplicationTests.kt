660ecdd61e1231a6867a8c3c8046ad7ca09b69a5
Jakub Śpiewak <priv@jakubspiewak.com>
2022-10-09 23:47:23 +0200

    feat: create backend

---
+++ backend/src/test/kotlin/com/jakubspiewak/money/MoneyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jakubspiewak.money
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoneyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
